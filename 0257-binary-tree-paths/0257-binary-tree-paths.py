3d9068097073c17f796289ab0fb8526a74d27367
Sediqa-Fahimi <sediqa.fahimi@gmail.com>
2023-02-01 15:42:51 -0800

    Time: 32 ms (84.20%), Space: 13.9 MB (68.77%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.py
+++ 0257-binary-tree-paths/0257-binary-tree-paths.py
@@ -5,22 +5,40 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+#     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]: # O(N) time | O(N) space [worst case: linear tree] O(logN) space [best case: balanced tree]
+#         paths = []
+        
+#         def dfs(root, path):
+#             if root:
+#                 path += str(root.val)
+                
+#                 if not root.left and not root.right:
+#                     paths.append(path)
+#                 else:
+#                     path += '->'
+#                     dfs(root.left,path)
+#                     dfs(root.right,path)
+                
+#         dfs(root, '')
+        
+#         return paths
+    
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        if not root: 
+            return []
         
+        stack = [(root, str(root.val))]
         paths = []
-        
-        def dfs(root, path):
-            if root:
-                path += str(root.val)
+        while stack:
+            node, path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
                 
-                if not root.left and not root.right:
-                    paths.append(path)
-                else:
-                    path += '->'
-                    dfs(root.left,path)
-                    dfs(root.right,path)
+            if node.left:
+                stack.append((node.left, path + '->' + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + '->' + str(node.right.val)))
                 
-        dfs(root, '')
         
         return paths
     
