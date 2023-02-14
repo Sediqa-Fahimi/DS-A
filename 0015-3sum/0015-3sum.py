277589b9b897467d2ee4c50c99d25bd42ed2dffe
Sediqa-Fahimi <sediqa.fahimi@gmail.com>
2023-02-13 16:41:42 -0800

    Time: 2595 ms (30.54%), Space: 18.2 MB (66.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -2,18 +2,21 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         results = []
         nums.sort()
-        curr = 0
+        i = 0
         curr_sum = 0
-        while curr < len(nums) - 2:
-            left = curr + 1
+        while i < len(nums) - 2:
+            if nums[i] > 0:
+                break
+                
+            left = i + 1
             right = len(nums) - 1
-            if curr > 0 and nums[curr] == nums[curr-1]:
-                curr += 1
+            if i > 0 and nums[i] == nums[i-1]:
+                i += 1
                 continue
             while left < right:
-                curr_sum = nums[curr] + nums[left] + nums[right]
+                curr_sum = nums[i] + nums[left] + nums[right]
                 if curr_sum == 0:
-                    results.append([nums[curr], nums[left], nums[right]])
+                    results.append([nums[i], nums[left], nums[right]])
                     left += 1
                     right -= 1
                 elif curr_sum < 0:
@@ -21,13 +24,13 @@ class Solution:
                 else:
                     right -= 1
                     
-                while curr + 1 != left and nums[left] == nums[left-1] and left < right:
+                while i + 1 != left and nums[left] == nums[left-1] and left < right:
                     left += 1
                 while right < len(nums) - 2 and nums[right] == nums[right+1] and left < right:
                     right -= 1
                     
                 curr_sum = 0
-            curr += 1
+            i += 1
         
         return results
    
