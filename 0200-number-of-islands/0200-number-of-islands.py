e85f21ff1112489dc5d2b61c3922c8f7119f313e
Sediqa-Fahimi <sediqa.fahimi@gmail.com>
2023-02-16 21:48:41 -0800

    Time: 334 ms (51.26%), Space: 16.5 MB (63.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,25 +2,27 @@ class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         # counter
         counter = 0
-        visited = set()
+        #visited = set()
         # traverse grid
         for row in range(len(grid)):
             for col in range(len(grid[row])):
-                if grid[row][col] == '1' and (row,col) not in visited:
+                if grid[row][col] == '1': # and (row,col) not in visited:
                     counter += 1 
-                    self.dfs(grid,row,col, visited)
+                    self.dfs(grid, row, col) #, visited)
                         
         
         return counter
     
 
-    def dfs(self, grid, row, col, visited):
-        visited.add((row,col))
+    def dfs(self, grid, row, col): #, visited):
+        #visited.add((row,col))
+        grid[row][col] = '0'
         directions = [[-1,0],[1, 0],[0,-1],[0,1]]
         for dir in directions:
             newRow = row + dir[0]
             newCol = col + dir[1]
-            if (newRow >= 0 and newRow < len(grid)) and (newCol >= 0 and newCol < len(grid[0])) and (grid[newRow][newCol] == '1' and (newRow, newCol) not in visited):
-                visited.add((newRow, newCol))
-                self.dfs(grid, newRow, newCol, visited)
+            if (newRow >= 0 and newRow < len(grid)) and (newCol >= 0 and newCol < len(grid[0])) and grid[newRow][newCol] == '1': #and (newRow, newCol) not in visited):
+                #visited.add((newRow, newCol))
+                grid[newRow][newCol] = '0'
+                self.dfs(grid, newRow, newCol) #, visited)
             
