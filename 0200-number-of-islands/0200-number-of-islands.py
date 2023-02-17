c704e6260af0629d7dbe1955b401f8cac5ebee8c
Sediqa-Fahimi <sediqa.fahimi@gmail.com>
2023-02-16 21:58:07 -0800

    Time: 362 ms (40.50%), Space: 16.5 MB (63.79%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,28 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        # counter
         counter = 0
-        #visited = set()
         # traverse grid
         for row in range(len(grid)):
             for col in range(len(grid[row])):
-                if grid[row][col] == '1': # and (row,col) not in visited:
+                if grid[row][col] == '1': 
                     counter += 1 
-                    self.dfs(grid, row, col) #, visited)
+                    self.dfs(grid, row, col) 
                         
         
         return counter
     
 
-    def dfs(self, grid, row, col): #, visited):
-        #visited.add((row,col))
+    def dfs(self, grid, row, col): 
+       
         grid[row][col] = '0'
         directions = [[-1,0],[1, 0],[0,-1],[0,1]]
         for dir in directions:
             newRow = row + dir[0]
             newCol = col + dir[1]
-            if (newRow >= 0 and newRow < len(grid)) and (newCol >= 0 and newCol < len(grid[0])) and grid[newRow][newCol] == '1': #and (newRow, newCol) not in visited):
-                #visited.add((newRow, newCol))
+            if self.inBound(grid, newRow, newCol) and grid[newRow][newCol] == '1':
                 grid[newRow][newCol] = '0'
-                self.dfs(grid, newRow, newCol) #, visited)
+                self.dfs(grid, newRow, newCol) 
+        
+        
+    def inBound(self, grid, i, j):
+        return (i >= 0 and i < len(grid)) and (j >= 0 and j < len(grid[0]))
+        
             
